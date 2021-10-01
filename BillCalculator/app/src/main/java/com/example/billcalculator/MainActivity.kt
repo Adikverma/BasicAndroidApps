980d3e23ddd832786d2a2f62d7e30ca4a462ec02
Aditya Kumar Verma <adikverma1@gmail.com>
2021-10-02 00:00:38 +0530

    bill calculator app layout completed

--- BillCalculator/app/src/main/java/com/example/billcalculator/MainActivity.kt
+++ BillCalculator/app/src/main/java/com/example/billcalculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.billcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPayment:Button=findViewById(R.id.btnPayment)
+        
+//        btnPayment.setOnClickListener {
+
+//        }
     }
 }
